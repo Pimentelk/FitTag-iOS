de7cc571c4a8cd2f23e1f45fbae1849232b95aa0
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.local>
2014-06-12 05:50:22 -0400

    Initial Commit

---
+++ FitTagTests/FitTagTests.m
@@ -0,0 +1,34 @@
+//
+//  FitTagTests.m
+//  FitTagTests
+//
+//  Created by Kevin Pimentel on 6/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FitTagTests : XCTestCase
+
+@end
+
+@implementation FitTagTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
