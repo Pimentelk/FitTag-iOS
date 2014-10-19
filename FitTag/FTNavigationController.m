eeef8d56d7eec6c70fc060b8f5f587150be9ae16
Kevin Pimentel <kevinpimentel@kevins-mac-mini.home>
2014-10-19 18:36:09 -0400

    - Added add friends  functionality to the FTNavigationController to prevent code duplication.

--- FitTag/FTNavigationController.m
+++ FitTag/FTNavigationController.m
@@ -8,6 +8,8 @@
 
 #import "FTNavigationController.h"
 #import "FTCamViewController.h"
+#import "FTFindFriendsViewController.h"
+#import "FTFindFriendsFlowLayout.h"
 
 #define VIEWCONTROLLER_NAVIGATION @"FTNavigationController"
 
@@ -17,6 +19,14 @@
     NSLog(@"%@::initWithRootViewController:",VIEWCONTROLLER_NAVIGATION);
     self = [super initWithRootViewController:rootViewController];
     if (self) {
+        // UIBarbutton items
+        UIBarButtonItem *addFriendsButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:NAVIGATION_BAR_BUTTON_ADD_CONTACTS]
+                                                                                 style:UIBarButtonItemStylePlain target:self
+                                                                                action:@selector(didTapAddFriendsButtonAction:)];
+        
+        [addFriendsButtonItem setTintColor:[UIColor whiteColor]];
+        rootViewController.navigationItem.leftBarButtonItem = addFriendsButtonItem;
+        
         // Initialization code
         UIBarButtonItem *loadCameraButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:NAVIGATION_BAR_BUTTON_CAMERA]
                                                                              style:UIBarButtonItemStylePlain
@@ -38,8 +48,24 @@
                                                       alpha:1.0f];
 }
 
+#pragma mark - ()
+
+- (void)didTapAddFriendsButtonAction:(UIButton *)button {
+    // Layout param
+    FTFindFriendsFlowLayout *layoutFlow = [[FTFindFriendsFlowLayout alloc] init];
+    [layoutFlow setItemSize:CGSizeMake(320,42)];
+    [layoutFlow setScrollDirection:UICollectionViewScrollDirectionVertical];
+    [layoutFlow setMinimumInteritemSpacing:0];
+    [layoutFlow setMinimumLineSpacing:0];
+    [layoutFlow setSectionInset:UIEdgeInsetsMake(0.0f,0.0f,0.0f,0.0f)];
+    [layoutFlow setHeaderReferenceSize:CGSizeMake(320,32)];
+    
+    // Show the interests
+    FTFindFriendsViewController *fiendFriendsViewController = [[FTFindFriendsViewController alloc] init];
+    [self pushViewController:fiendFriendsViewController animated:YES];
+}
+
 - (void)didTapLoadCameraButtonAction:(UIButton *)button {
-    // Map Home View
     FTCamViewController *camViewController = [[FTCamViewController alloc] init];
     UINavigationController *navController = [[UINavigationController alloc] init];
     [navController setViewControllers:@[camViewController] animated:NO];
