02d07b8f147f0daa8e34a20b1ca83d2630a25687
Kevin Pimentel <kevinpimentel@kevins-mac-mini.home>
2014-10-21 23:16:14 -0400

    - Added FTSettingsViewController. - This is a table view controller that displays all settings options. - Data is in the form of an NSDictionary where headers are keys and value (an array) are the header sub categories.

---
+++ FitTag/FTSettingsViewController.h
@@ -0,0 +1,19 @@
+//
+//  FTSettingsActionSheetDelegate.h
+//  FitTag
+//
+//  Created by Kevin Pimentel on 7/26/14.
+//  Copyright (c) 2014 Kevin Pimentel. All rights reserved.
+//
+
+#import "FTSettingsDetailViewController.h"
+#import "FTInterestsViewController.h"
+#import "FTFindFriendsViewController.h"
+#import <MessageUI/MessageUI.h>
+
+@interface FTSettingsViewController : UITableViewController <UITableViewDataSource,UITableViewDelegate,MFMailComposeViewControllerDelegate>
+
+@property (strong, nonatomic) FTSettingsDetailViewController *settingsDetailViewController;
+@property (strong, nonatomic) FTFindFriendsViewController *findFriendsViewController;
+@property (strong, nonatomic) FTInterestsViewController *interestsViewController;
+@end
