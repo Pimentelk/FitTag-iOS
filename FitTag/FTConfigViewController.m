c2557dac1535de40a5dfa4b13498fab23c069260
Kevin Pimentel <kevinpimentel@kevins-mac-mini.home>
2014-10-26 02:18:54 -0400

    - Removed the Facebook and twitter data sync functionality from this class. We don't want this to run every time the user signs in, instead we only want it the first time the login.

--- FitTag/FTConfigViewController.m
+++ FitTag/FTConfigViewController.m
@@ -82,6 +82,7 @@
 
 #pragma mark - DidLoginWithSocialMedia
 
+/*
 - (BOOL)isLoggedInWithTwitter {
     //NSLog(@"%@::isLoggedInWithTwitter:",VIEWCONTROLLER_CONFIG);
     return [PFTwitterUtils isLinkedWithUser:[PFUser currentUser]];
@@ -91,98 +92,7 @@
     //NSLog(@"%@::isLoggedInWithFacebook:",VIEWCONTROLLER_CONFIG);
     return [PFFacebookUtils isLinkedWithUser:[PFUser currentUser]];
 }
-
-- (BOOL)didLogInWithTwitter:(PFObject *)user {
-    //NSLog(@"%@::didLogInWithTwitter:",VIEWCONTROLLER_CONFIG);
-    if ([self isLoggedInWithTwitter]) {
-        NSLog(USER_DID_LOGIN_TWITTER);
-        NSString * requestString = [NSString stringWithFormat:TWITTER_API_USERS,[PFTwitterUtils twitter].screenName];
-        NSURL *verify = [NSURL URLWithString:requestString];
-        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:verify];
-        [[PFTwitterUtils twitter] signRequest:request];
-        NSURLResponse *response = nil;
-        NSError *error = nil;
-        NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-        
-        if (error == nil){
-            NSDictionary* TWuser = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
-            NSString *profile_image_normal = [TWuser objectForKey:TWITTER_PROFILE_HTTPS];
-            NSString *profile_image = [profile_image_normal stringByReplacingOccurrencesOfString:@"_normal" withString:@""];
-            NSData *profileImageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:profile_image]];
-            
-            NSString *names = [TWuser objectForKey:@"name"];
-            NSMutableArray *array = [NSMutableArray arrayWithArray:[names componentsSeparatedByString:@" "]];
-            
-            if (array.count > 1){
-                [user setObject:[array lastObject] forKey:kFTUserLastnameKey];
-                [array removeLastObject];
-                [user setObject:[array componentsJoinedByString:@" "] forKey:kFTUserFirstnameKey];
-            }
-            
-            [user setValue:[TWuser objectForKey:@"name"]
-                    forKey:kFTUserDisplayNameKey];
-            
-            [user setValue:[NSString stringWithFormat:@"%@",[TWuser objectForKey:@"id"]]
-                    forKey:kFTUserTwitterIdKey];
-            
-            //[user setValue:DEFAULT_BIO_TEXT_B forKey:kFTUserBioKey];
-            
-            [user setValue:[PFFile fileWithName:MEDIUM_JPEG data:profileImageData]
-                    forKey:kFTUserProfilePicMediumKey];
-            
-            [user setValue:[PFFile fileWithName:SMALL_JPEG data:profileImageData]
-                    forKey:kFTUserProfilePicSmallKey];
-  
-            [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-                if (error) {
-                    NSLog(@"%@%@",ERROR_MESSAGE,error);
-                    [user saveEventually];
-                }
-            }];
-        }
-        return YES;
-    }
-
-    NSLog(USER_NOT_LOGIN_TWITTER);
-    return NO;
-}
-
-- (BOOL)didLogInWithFacebook:(PFObject *)user {
-    NSLog(@"%@::didLogInWithFacebook:",VIEWCONTROLLER_CONFIG);
-
-    if ([self isLoggedInWithFacebook]) {
-        NSLog(USER_DID_LOGIN_FACEBOOK);
-        
-        [[FBRequest requestForMe] startWithCompletionHandler:^(FBRequestConnection *connection, NSDictionary<FBGraphUser> *FBuser, NSError *error) {
-            if (!error) {
-                NSData* profileImageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:[NSString stringWithFormat:FACEBOOK_GRAPH_PICTURES_URL,[FBuser objectForKey:FBUserIDKey]]]];
-                
-                // Get the data from facebook and put it into the user object
-                [user setValue:[FBuser objectForKey:FBUserFirstNameKey] forKey:kFTUserFirstnameKey];
-                [user setValue:[FBuser objectForKey:FBUserLastNameKey] forKey:kFTUserLastnameKey];
-                [user setValue:[FBuser objectForKey:FBUserNameKey] forKey:kFTUserDisplayNameKey];
-                [user setValue:[FBuser objectForKey:FBUserEmailKey] forKey:kFTUserEmailKey];
-                [user setValue:[FBuser objectForKey:FBUserIDKey] forKey:kFTUserFacebookIDKey];
-                //[user setValue:DEFAULT_BIO_TEXT_B forKey:kFTUserBioKey];
-                [user setValue:[PFFile fileWithName:MEDIUM_JPEG data:profileImageData] forKey:kFTUserProfilePicMediumKey];
-                [user setValue:[PFFile fileWithName:SMALL_JPEG data:profileImageData] forKey:kFTUserProfilePicSmallKey];
-                [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-                    if (error) {
-                        [user saveEventually];
-                        NSLog(@"%@ %@", ERROR_MESSAGE, error);
-                    }
-                }];
-                
-            } else {
-                NSLog(@"Facebook%@%@",ERROR_MESSAGE,error);
-            }
-        }];
-        return YES;
-    }
-    
-    NSLog(@"%@ %@",ERROR_MESSAGE,USER_NOT_LOGIN_FACEBOOK);
-    return NO;
-}
+*/
 
 - (void)presentTabBarController:(PFUser *)user {
     
@@ -217,8 +127,6 @@
 
 - (void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user {
     //NSLog(@"%@::logInViewController:didLogInUser:",VIEWCONTROLLER_CONFIG);
-    [self didLogInWithFacebook:user];
-    [self didLogInWithTwitter:user];
     [self presentTabBarController:user];
 }
 
@@ -279,7 +187,7 @@ shouldBeginLogInWithUsername:(NSString *)username
         }
     }
     
-    if (!informationComplete || !isPasswordConfirmed || [lastname isEqualToString:@""] || [firstname isEqualToString:@""]) {
+    if (!informationComplete || !isPasswordConfirmed || [lastname isEqualToString:EMPTY_STRING] || [firstname isEqualToString:EMPTY_STRING]) {
         NSLog(@"Missing Information");
             [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Missing Information", nil)
                                         message:NSLocalizedString(STRING_MISSING_INFORMATION, nil)
@@ -302,7 +210,7 @@ shouldBeginLogInWithUsername:(NSString *)username
         [user setValue:signUpViewController.lastname forKey:kFTUserLastnameKey];
         [user setValue:[NSString stringWithFormat:@"@%@%@",signUpViewController.firstname,signUpViewController.lastname] forKey:kFTUserDisplayNameKey];
         
-        if (![signUpViewController.about isEqualToString:@""]){
+        if (![signUpViewController.about isEqualToString:EMPTY_STRING]){
             [user setValue:signUpViewController.about forKey:kFTUserBioKey];
         } else {
             [user setValue:DEFAULT_BIO_TEXT_A forKey:kFTUserBioKey];
