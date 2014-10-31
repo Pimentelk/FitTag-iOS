bd5f0c139b121bff4a210be99f3146450df7b81d
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-10-31 02:26:15 -0400

    - Renamed the FTSocialCell files to FTSwitchCell - This file now handles an additional four types:     FTSwitchTypeComment = 1 << 2,     FTSwitchTypeLike = 1 << 3,     FTSwitchTypeFollow = 1 << 4,     FTSwitchTypeMention = 1 << 5,

---
+++ FitTag/FTSwitchCell.m
@@ -0,0 +1,146 @@
+//
+//  UITableViewCell+FTSocialCell.m
+//  FitTag
+//
+//  Created by Kevin Pimentel on 10/25/14.
+//  Copyright (c) 2014 Kevin Pimentel. All rights reserved.
+//
+
+#import "FTSwitchCell.h"
+
+@interface FTSwitchCell () {
+    UISwitch *settingSwitch;
+}
+@end
+
+@implementation FTSwitchCell
+@synthesize delegate;
+
+- (id)initWithStyle:(UITableViewCellStyle)style
+    reuseIdentifier:(NSString *)reuseIdentifier {
+    
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        [self.contentView setBackgroundColor:[UIColor clearColor]];
+        settingSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(self.frame.size.width - 80, 5, 0, 0)];
+    }
+    return self;
+}
+
+- (void)setType:(FTSwitchType)type {
+    
+    // Remove target
+    [settingSwitch removeTarget:self action:@selector(didChangeFacebookSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+    [settingSwitch removeTarget:self action:@selector(didChangeTwitterSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+    [settingSwitch removeTarget:self action:@selector(didChangeCommentSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+    [settingSwitch removeTarget:self action:@selector(didChangeLikeSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+    [settingSwitch removeTarget:self action:@selector(didChangeFollowSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+    [settingSwitch removeTarget:self action:@selector(didChangeMentionSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Set target
+    switch (type) {
+        case FTSwitchTypeFacebook: {
+            if ([PFFacebookUtils isLinkedWithUser:[PFUser currentUser]]) {
+                [settingSwitch setOn:YES animated:YES];
+            } else {
+                [settingSwitch setOn:NO animated:YES];
+            }
+            [settingSwitch addTarget:self action:@selector(didChangeFacebookSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+        }
+            break;
+        case FTSwitchTypeTwitter: {
+            if ([PFTwitterUtils isLinkedWithUser:[PFUser currentUser]]) {
+                [settingSwitch setOn:YES animated:YES];
+            } else {
+                [settingSwitch setOn:NO animated:YES];
+            }
+            [settingSwitch addTarget:self action:@selector(didChangeTwitterSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+        }
+            break;
+        case FTSwitchTypeComment: {
+            if(![[NSUserDefaults standardUserDefaults] boolForKey:kFTUserDefaultsSettingsViewControllerPushCommentsKey]) {
+                [settingSwitch setOn:NO animated:YES];
+            } else {
+                [settingSwitch setOn:YES animated:YES];
+            }
+            [settingSwitch addTarget:self action:@selector(didChangeCommentSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+        }
+            break;
+        case FTSwitchTypeLike: {
+            if(![[NSUserDefaults standardUserDefaults] boolForKey:kFTUserDefaultsSettingsViewControllerPushLikesKey]) {
+                [settingSwitch setOn:NO animated:YES];
+            } else {
+                [settingSwitch setOn:YES animated:YES];
+            }
+            [settingSwitch addTarget:self action:@selector(didChangeLikeSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+        }
+            break;
+        case FTSwitchTypeFollow: {
+            if(![[NSUserDefaults standardUserDefaults] boolForKey:kFTUserDefaultsSettingsViewControllerPushFollowsKey]) {
+                [settingSwitch setOn:NO animated:YES];
+            } else {
+                [settingSwitch setOn:YES animated:YES];
+            }
+            [settingSwitch addTarget:self action:@selector(didChangeFollowSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+        }
+            break;
+        case FTSwitchTypeMention: {
+            if(![[NSUserDefaults standardUserDefaults] boolForKey:kFTUserDefaultsSettingsViewControllerPushMentionsKey]) {
+                [settingSwitch setOn:NO animated:YES];
+            } else {
+                [settingSwitch setOn:YES animated:YES];
+            }
+            [settingSwitch addTarget:self action:@selector(didChangeMentionSwitchAction:) forControlEvents:UIControlEventTouchUpInside];
+        }
+            break;
+        case FTSwitchTypeNone: {
+            
+        }
+            break;
+    }    
+    [self.contentView addSubview:settingSwitch];
+}
+
+- (void)didChangeFacebookSwitchAction:(UISwitch *)lever {
+    NSLog(@"didChangeFacebookSwitchAction");
+    if (delegate && [delegate respondsToSelector:@selector(switchCell:didChangeFacebookSwitch:)]) {
+        [delegate switchCell:self didChangeFacebookSwitch:lever];
+    }
+}
+
+- (void)didChangeTwitterSwitchAction:(UISwitch *)lever {
+    NSLog(@"didChangeTwitterSwitchAction");
+    if (delegate && [delegate respondsToSelector:@selector(switchCell:didChangeTwitterSwitch:)]) {
+        [delegate switchCell:self didChangeTwitterSwitch:lever];
+    }
+}
+
+- (void)didChangeCommentSwitchAction:(UISwitch *)lever {
+    NSLog(@"didChangeCommentSwitchAction");
+    if (delegate && [delegate respondsToSelector:@selector(switchCell:didChangeCommentSwitch:)]) {
+        [delegate switchCell:self didChangeCommentSwitch:lever];
+    }
+}
+
+- (void)didChangeLikeSwitchAction:(UISwitch *)lever {
+    NSLog(@"didChangeLikeSwitchAction");
+    if (delegate && [delegate respondsToSelector:@selector(switchCell:didChangeLikeSwitch:)]) {
+        [delegate switchCell:self didChangeLikeSwitch:lever];
+    }
+}
+
+- (void)didChangeFollowSwitchAction:(UISwitch *)lever {
+    NSLog(@"didChangeFollowSwitchAction");
+    if (delegate && [delegate respondsToSelector:@selector(switchCell:didChangeFollowSwitch:)]) {
+        [delegate switchCell:self didChangeFollowSwitch:lever];
+    }
+}
+
+- (void)didChangeMentionSwitchAction:(UISwitch *)lever {
+    NSLog(@"didChangeMentionSwitchAction");
+    if (delegate && [delegate respondsToSelector:@selector(switchCell:didChangeMentionSwitch:)]) {
+        [delegate switchCell:self didChangeMentionSwitch:lever];
+    }
+}
+
+@end
