0f8e2dd3381b5e281167ed5ec7ea026c72f99455
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-11-02 22:38:44 -0500

    Cache policy updated to check network
    Query will check the cache first, then the network.

--- FitTag/FTInviteFriendsViewController.m
+++ FitTag/FTInviteFriendsViewController.m
@@ -71,7 +71,7 @@
     PFQuery *followingActivitiesQuery = [PFQuery queryWithClassName:kFTActivityClassKey];
     [followingActivitiesQuery whereKey:kFTActivityTypeKey equalTo:kFTActivityTypeFollow];
     [followingActivitiesQuery whereKey:kFTActivityFromUserKey equalTo:[PFUser currentUser]];
-    [followingActivitiesQuery setCachePolicy:kPFCachePolicyNetworkOnly];
+    [followingActivitiesQuery setCachePolicy:kPFCachePolicyCacheThenNetwork];
     [followingActivitiesQuery includeKey:kFTActivityToUserKey];
     [followingActivitiesQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
         if (!error) {
