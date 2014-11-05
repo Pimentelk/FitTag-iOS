75c511c2e88c4577ae1d3210a9703d6875af29d7
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-11-05 09:43:40 -0500

    Add clear checks to prevent logic errors. Split the signup checks into their own blocks to prevent any "fuzzy logic"

--- FitTag/FTConfigViewController.m
+++ FitTag/FTConfigViewController.m
@@ -143,77 +143,145 @@ shouldBeginLogInWithUsername:(NSString *)username
            shouldBeginSignUp:(NSDictionary *)info {
     NSLog(@"%@::signUpViewController:shouldBeginSignUp:",VIEWCONTROLLER_CONFIG);
     
-    NSString *firstname = signUpViewController.firstname;
-    NSString *lastname = signUpViewController.lastname;
-    BOOL isPasswordConfirmed = signUpViewController.isPasswordConfirmed;
-    BOOL informationComplete = YES;
-    for (id key in info) {
-        NSString *field = [info objectForKey:key];
-        if (!field || field.length == 0) {
-            informationComplete = NO;
-            break;
+    if (signUpViewController) {
+        
+        NSString *firstname = EMPTY_STRING;
+        NSString *lastname = EMPTY_STRING;
+        BOOL isPasswordConfirmed = NO;
+        BOOL informationComplete = YES;
+        
+        if (signUpViewController.firstname) {
+            NSLog(@"firstname is valid signUpViewController.firstname");
+            firstname = signUpViewController.firstname;
         }
-    }
-    
-    if (!informationComplete || !isPasswordConfirmed || [lastname isEqualToString:EMPTY_STRING] || [firstname isEqualToString:EMPTY_STRING]) {
-        NSLog(@"Missing Information");
-            [[[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Missing Information", nil)
-                                        message:NSLocalizedString(STRING_MISSING_INFORMATION, nil)
+        
+        if (signUpViewController.lastname) {
+            NSLog(@"lastname is valid signUpViewController.lastname");
+            lastname = signUpViewController.lastname;
+        }
+        
+        if (signUpViewController.isPasswordConfirmed) {
+            NSLog(@"isPasswordConfirmed is valid signUpViewController.isPasswordConfirmed");
+            isPasswordConfirmed = signUpViewController.isPasswordConfirmed;
+        }
+        
+        for (id key in info) {
+            NSString *field = [info objectForKey:key];
+            if (!field || field.length == 0) {
+                informationComplete = NO;
+                break;
+            }
+        }
+        
+        // If user information is not complete email | username | password are missing
+        if (!informationComplete) {
+            NSLog(@"user information is not complete");
+            [[[UIAlertView alloc] initWithTitle:NSLocalizedString(MESSAGE_TITTLE_MISSING_INFO, nil)
+                                        message:NSLocalizedString(MESSAGE_MISSING_INFORMATION, nil)
                                        delegate:nil
                               cancelButtonTitle:NSLocalizedString(@"OK", nil)
                               otherButtonTitles:nil] show];
-        return NO;
+            
+            return NO;
+        }
+        
+        // If password confirm does not match
+        if (!isPasswordConfirmed) {
+            NSLog(@"If password confirm does not match");
+            [[[UIAlertView alloc] initWithTitle:NSLocalizedString(MESSAGE_TITTLE_MISSING_INFO, nil)
+                                        message:NSLocalizedString(MESSAGE_CONFIRMED_MATCH, nil)
+                                       delegate:nil
+                              cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                              otherButtonTitles:nil] show];
+            
+            return NO;
+        }
+        
+        // If firstname or lastname are empty
+        if (lastname.length <= 0 && firstname.length <= 0) {
+            NSLog(@"If firstname or lastname are empty");
+            [[[UIAlertView alloc] initWithTitle:NSLocalizedString(MESSAGE_TITTLE_MISSING_INFO, nil)
+                                        message:NSLocalizedString(MESSAGE_NAME_EMPTY, nil)
+                                       delegate:nil
+                              cancelButtonTitle:NSLocalizedString(@"OK", nil)
+                              otherButtonTitles:nil] show];
+            return NO;
+        }
+        return YES;
     }
-    
-    return YES;
+    return NO;
 }
 
 // Sent to the delegate when a PFUser is signed up.
-- (void)signUpViewController:(PFSignUpViewController *)signUpController didSignUpUser:(PFUser *)user {
-    //NSLog(@"%@::signUpViewController:didSignUpUser:",VIEWCONTROLLER_CONFIG);
+- (void)signUpViewController:(PFSignUpViewController *)signUpController
+               didSignUpUser:(PFUser *)user {
+    NSLog(@"%@::signUpViewController:didSignUpUser:",VIEWCONTROLLER_CONFIG);
     if (user){
         NSLog(USER_DID_LOGIN);
-        [user setValue:signUpViewController.firstname forKey:kFTUserFirstnameKey];
-        [user setValue:signUpViewController.lastname forKey:kFTUserLastnameKey];
-        [user setValue:[NSString stringWithFormat:@"@%@%@",signUpViewController.firstname,signUpViewController.lastname] forKey:kFTUserDisplayNameKey];
-        
-        if (![signUpViewController.about isEqualToString:EMPTY_STRING]){
-            [user setValue:signUpViewController.about forKey:kFTUserBioKey];
-        } else {
-            [user setValue:DEFAULT_BIO_TEXT_A forKey:kFTUserBioKey];
-        }
-        
-        if (signUpViewController.profilePhoto) {
-            UIImage *resizedImage = [signUpViewController.profilePhoto resizedImageWithContentMode:UIViewContentModeScaleAspectFit
-                                                                                            bounds:CGSizeMake(560.0f, 560.0f)
-                                                                              interpolationQuality:kCGInterpolationHigh];
-            
-            UIImage *thumbImage = [signUpViewController.profilePhoto thumbnailImage:86.0f
-                                                                  transparentBorder:0.0f
-                                                                       cornerRadius:10.0f
-                                                               interpolationQuality:kCGInterpolationDefault];
-            
-            NSData *imageData           = UIImageJPEGRepresentation(resizedImage, 0.8f);
-            NSData *thumbnailImageData  = UIImagePNGRepresentation(thumbImage);
-            
-            if (imageData && thumbnailImageData) {
-                [user setValue:[PFFile fileWithName:FILE_MEDIUM_JPEG data:imageData] forKey:kFTUserProfilePicMediumKey];
-                [user setValue:[PFFile fileWithName:FILE_SMALL_JPEG data:imageData] forKey:kFTUserProfilePicSmallKey];
-            }
-        }
-        
-        [user setValue:kFTUserTypeUser forKey:kFTUserTypeKey];
-        [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-            if (error) {
-                [user saveEventually];
-                NSLog(@"%@%@",ERROR_MESSAGE,error);
-            }
-        }];
         
         if (signUpViewController) {
-            [signUpViewController dismissViewControllerAnimated:YES completion:nil];
-            [self presentTabBarController:user];
+            
+            if (signUpViewController.firstname) {
+                NSLog(@"signUpViewController.firstname");
+                [user setValue:signUpViewController.firstname forKey:kFTUserFirstnameKey];
+            }
+            
+            if (signUpViewController.lastname) {
+                NSLog(@"signUpViewController.lastname");
+                [user setValue:signUpViewController.lastname forKey:kFTUserLastnameKey];
+            }
+            
+            if (signUpViewController.firstname && signUpViewController.lastname) {
+                NSLog(@"signUpViewController.firstname && signUpViewController.lastname");
+                [user setValue:[NSString stringWithFormat:@"@%@%@",signUpViewController.firstname,signUpViewController.lastname] forKey:kFTUserDisplayNameKey];
+            }
+            
+            [user setValue:DEFAULT_BIO_TEXT_A forKey:kFTUserBioKey];
+            
+            if (signUpViewController.about.length > 0){
+                NSLog(@"signUpViewController.about.length");
+                [user setValue:signUpViewController.about forKey:kFTUserBioKey];
+            }
+            
+            if (signUpViewController.profilePhoto) {
+                NSLog(@"signUpViewController.profilePhoto");
+                
+                UIImage *signupProfilePhoto = signUpViewController.profilePhoto;
+                
+                UIImage *resizedImage = [signupProfilePhoto resizedImageWithContentMode:UIViewContentModeScaleAspectFit
+                                                                                 bounds:CGSizeMake(560.0f, 560.0f)
+                                                                   interpolationQuality:kCGInterpolationHigh];
+                
+                UIImage *thumbImage = [signupProfilePhoto thumbnailImage:86.0f
+                                                       transparentBorder:0.0f
+                                                            cornerRadius:10.0f
+                                                    interpolationQuality:kCGInterpolationDefault];
+                
+                NSData *imageData           = UIImageJPEGRepresentation(resizedImage, 0.8f);
+                NSData *thumbnailImageData  = UIImagePNGRepresentation(thumbImage);
+                
+                if (imageData && thumbnailImageData) {
+                    [user setValue:[PFFile fileWithName:FILE_MEDIUM_JPEG data:imageData] forKey:kFTUserProfilePicMediumKey];
+                    [user setValue:[PFFile fileWithName:FILE_SMALL_JPEG data:thumbnailImageData] forKey:kFTUserProfilePicSmallKey];
+                }
+            }
+            
+            [user setValue:kFTUserTypeUser forKey:kFTUserTypeKey];
+            
+            NSLog(@"user:%@",user);
+            [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (error) {
+                    [user saveEventually];
+                    NSLog(@"%@%@",ERROR_MESSAGE,error);
+                }
+            }];
+            
+            if (signUpViewController) {
+                [signUpViewController dismissViewControllerAnimated:YES completion:nil];
+                [self presentTabBarController:user];
+            }
         }
+        
     } else {
         NSLog(USER_NOT_LOGGEDIN);
     }
