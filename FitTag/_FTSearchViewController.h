c826c0c0ede84664de525466c0b5831779c9a261
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-11-07 14:54:31 -0500

    Renamed these FTSearch VC Files, getting ready to delete once confirmed that I no longer need them. New FTSearch VC incoming.

---
+++ FitTag/_FTSearchViewController.h
@@ -0,0 +1,16 @@
+//
+//  FTSearchResultsViewController.h
+//  FitTag
+//
+//  Created by Kevin Pimentel on 9/2/14.
+//  Copyright (c) 2014 Kevin Pimentel. All rights reserved.
+//
+/*
+#import "FTSearchCell.h"
+#import "FTSearchHeaderView.h"
+#import <CoreLocation/CoreLocation.h>
+
+@interface FTSearchViewController : PFQueryTableViewController <FTSearchHeaderViewDelegate,FTSearchCellDelegate,UIActionSheetDelegate,
+                                                                CLLocationManagerDelegate,UITextFieldDelegate,UISearchBarDelegate>
+@end
+*/
