bbfa81c15e1adea4db712729aa0cccb967fc36cf
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-11-08 09:29:48 -0500

    Add FTSearchQueryType enum - Displays the search terms based on user input - Extends FTTimelineViewController - Has it's own searchable that is populated with the search term

--- FitTag/FTSearchViewController.h
+++ FitTag/FTSearchViewController.h
@@ -1,15 +1,29 @@
 //
-//  FTSearchResultsViewController.h
+//  FTSearchViewController.h
 //  FitTag
 //
-//  Created by Kevin Pimentel on 9/2/14.
+//  Created by Kevin Pimentel on 11/6/14.
 //  Copyright (c) 2014 Kevin Pimentel. All rights reserved.
 //
 
 #import "FTSearchCell.h"
-#import "FTSearchHeaderView.h"
-#import <CoreLocation/CoreLocation.h>
+#import "FTTimelineViewController.h"
+#import "FTLoadMoreCell.h"
 
-@interface FTSearchViewController : PFQueryTableViewController <FTSearchHeaderViewDelegate,FTSearchCellDelegate,UIActionSheetDelegate,
-                                                                CLLocationManagerDelegate,UITextFieldDelegate,UISearchBarDelegate>
+typedef enum {
+    FTSearchQueryTypeNone = 0,
+    FTSearchQueryTypeUser = 1 << 0,
+    FTSearchQueryTypeTrending = 1 << 1,
+    FTSearchQueryTypeNearby = 1 << 2,
+    FTSearchQueryTypeHashtag = 1 << 3,
+    FTSearchQueryTypeMention = 1 << 4,
+    FTSearchQueryTypePopular = 1 << 5,
+    FTSearchQueryTypeFitTag = FTSearchQueryTypeHashtag,
+} FTSearchQueryType;
+
+@interface FTSearchViewController : FTTimelineViewController <UISearchBarDelegate>
+
+@property (nonatomic, assign) FTSearchQueryType searchQueryType;
+@property (nonatomic, strong) NSString *searchString;
+
 @end
