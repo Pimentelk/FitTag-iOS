476a725b4def124c45a7162d7675699fe222ae19
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-11-19 20:38:23 -0500

    Fix wrong super method being called in viewWillAppear

--- FitTag/FTUserProfileViewController.m
+++ FitTag/FTUserProfileViewController.m
@@ -87,7 +87,7 @@
 
 - (void)viewWillAppear:(BOOL)animated{
     //NSLog(@"%@::viewWillAppear:",VIEWCONTROLLER_USER);
-    [super viewWillDisappear:animated];
+    [super viewWillAppear:animated];
     [self.navigationController setToolbarHidden:YES];
 }
 
