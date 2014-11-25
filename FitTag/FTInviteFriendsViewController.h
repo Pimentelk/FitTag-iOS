e5da9aa73d36851074fdb3eaab776b6370d216f7
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-11-25 12:20:32 -0500

    FTInviteFriendsViewControllers were moved to FTFollowFriendsViewController and now committing the newer versions of the FTInviteFriendsViewController, as the names make more sense this way

--- FitTag/FTInviteFriendsViewController.h
+++ FitTag/FTInviteFriendsViewController.h
@@ -1,29 +1,11 @@
 //
-//  FTInviteFriendsViewController.h
+//  UITableView+FTInviteFriendsViewController.h
 //  FitTag
 //
-//  Created by Kevin Pimentel on 10/27/14.
+//  Created by Kevin Pimentel on 11/25/14.
 //  Copyright (c) 2014 Kevin Pimentel. All rights reserved.
 //
 
-typedef enum {
-    FTFollowUserQueryTypeNone = 0,
-    FTFollowUserQueryTypeNear = 1 << 0,
-    FTFollowUserQueryTypeInterest = 1 << 1,
-    FTFollowUserQueryTypeAmbassador = 1 << 2,
-    FTFollowUserQueryTypeBusiness = 1 << 3,
-    FTFollowUserQueryTypeUser = 1 << 4,
-    FTFollowUserQueryTypeTagger = FTFollowUserQueryTypeAmbassador | FTFollowUserQueryTypeBusiness | FTFollowUserQueryTypeUser,
-    FTFollowUserQueryTypeDefault = FTFollowUserQueryTypeNear
-} FTFollowUserQueryType;
-
-#import "FTFollowCell.h"
-#import "FTInviteTableHeaderView.h"
-
-@interface FTInviteFriendsViewController : UITableViewController <FTFollowCellDelegate,FTInviteTableHeaderViewDelegate>
-@property (nonatomic, assign) FTFollowUserQueryType followUserQueryType;
-@property (nonatomic, strong) NSString *searchString;
-
-- (void)querySearchForUser;
+@interface FTInviteFriendsViewController : UICollectionViewController
 
 @end
