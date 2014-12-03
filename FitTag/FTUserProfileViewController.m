831f9c1c5982d1ea1fb7bdd4e3e899a85b1e0940
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-12-02 21:09:18 -0500

    Set a default barTintColor of white for the title

--- FitTag/FTUserProfileViewController.m
+++ FitTag/FTUserProfileViewController.m
@@ -45,6 +45,14 @@
     
     [self.navigationController setNavigationBarHidden:NO animated:NO];
     
+    
+    self.navigationController.navigationBar.titleTextAttributes = [NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], NSForegroundColorAttributeName,nil];
+    
+    self.navigationController.navigationBar.barTintColor = [UIColor colorWithRed:234.0f/255.0f
+                                                                           green:37.0f/255.0f
+                                                                            blue:37.0f/255.0f
+                                                                           alpha:1.0f];
+    
     // Set Background
     [self.collectionView setBackgroundColor:[UIColor whiteColor]];
     
