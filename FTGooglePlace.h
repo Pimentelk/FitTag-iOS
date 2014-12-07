97eab218258a731aa74df73040a0102d990035ac
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-12-07 10:37:29 -0500

    Add FTGooglePlaces

---
+++ FTGooglePlace.h
@@ -0,0 +1,13 @@
+//
+//  FTGooglePlace.h
+//  FitTag
+//
+//  Created by Kevin Pimentel on 12/6/14.
+//  Copyright (c) 2014 Kevin Pimentel. All rights reserved.
+//
+
+
+
+@interface FTGooglePlace : NSObject
+
+@end
---
+++ FTGooglePlace.m
@@ -0,0 +1,13 @@
+//
+//  FTGooglePlace.m
+//  FitTag
+//
+//  Created by Kevin Pimentel on 12/6/14.
+//  Copyright (c) 2014 Kevin Pimentel. All rights reserved.
+//
+
+#import "FTGooglePlace.h"
+
+@implementation FTGooglePlace
+
+@end
