c5ce310319c9734f76f5f7a833c061ab95541f30
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2014-12-11 23:40:21 -0500

    Cleaned code, removed hard code CGSizeMake

--- FitTag/FTRewardsDetailView.m
+++ FitTag/FTRewardsDetailView.m
@@ -316,7 +316,7 @@
 - (void)rewardsDetailHeaderView:(FTRewardsDetailHeaderView *)rewardsDetailHeaderView didTapBusinessButton:(UIButton *)button business:(PFUser *)business {
     
     UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
-    [flowLayout setItemSize:CGSizeMake(105.5,105)];
+    [flowLayout setItemSize:CGSizeMake(self.view.frame.size.width/3,105)];
     [flowLayout setScrollDirection:UICollectionViewScrollDirectionVertical];
     [flowLayout setMinimumInteritemSpacing:0];
     [flowLayout setMinimumLineSpacing:0];
