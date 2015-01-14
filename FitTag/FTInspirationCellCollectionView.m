32db8976970f42a066bb3066dfe1975056f5d1f3
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2015-01-14 16:55:53 -0500

    Add font MULIREGULAR

--- FitTag/FTInspirationCellCollectionView.m
+++ FitTag/FTInspirationCellCollectionView.m
@@ -31,23 +31,19 @@
         [self addSubview:lineViewWhite];
         
         self.message = [[UILabel alloc] initWithFrame:CGRectMake(8, 5, 195, 40)];
-        //self.message = [[UILabel alloc] init];
         [self.message setTextAlignment: NSTextAlignmentLeft];
         [self.message setTextColor: [UIColor blackColor]];
-        [self.message setFont:BENDERSOLID(14)];
-        //[self.message setBackgroundColor:[UIColor blueColor]];
+        [self.message setFont:MULIREGULAR(14)];
         [self addSubview: self.message];
         
         self.messageInterests = [[UILabel alloc] initWithFrame:CGRectMake(self.message.frame.size.width, 5, frame.size.width - self.message.frame.size.width, 40)];
-        //self.messageInterests = [[UILabel alloc] init];
         [self.messageInterests setFont:[UIFont systemFontOfSize:11]];
         [self.messageInterests setTextAlignment: NSTextAlignmentLeft];
-        [self.messageInterests setTextColor: [UIColor redColor]];
-        //[self.messageInterests setBackgroundColor:[UIColor yellowColor]];
+        [self.messageInterests setTextColor: FT_RED];
         [self.messageInterests setAdjustsFontSizeToFitWidth:NO];
         [self.messageInterests setLineBreakMode:NSLineBreakByTruncatingHead];
         [self.messageInterests setNumberOfLines:0];
-        [self.messageInterests setFont:BENDERSOLID(14)];
+        [self.messageInterests setFont:MULIREGULAR(14)];
         [self.messageInterests sizeThatFits:CGSizeMake(self.message.frame.size.width, FLT_MAX)];
         [self addSubview: self.messageInterests];
         
@@ -55,10 +51,6 @@
         
         imageView = [[PFImageView alloc] initWithFrame:CGRectMake(8, 37, 52, 52)];
         imageView.backgroundColor = [UIColor clearColor];
-        
-        //UIImageView *profileHexagon = [FTUtility getProfileHexagonWithFrame:imageView.frame];
-        //imageView.frame = profileHexagon.frame;
-        //imageView.layer.mask = profileHexagon.layer.mask;
         imageView.frame = CGRectMake(9, 37, 52, 52);
         imageView.layer.cornerRadius = CORNERRADIUS(52);
         imageView.clipsToBounds = YES;
@@ -72,7 +64,7 @@
 - (void)setImage:(UIImage *)aImage {    
     
     if (!aImage) {
-        NSLog(@"image nil..");
+        //NSLog(@"image nil..");
         return;
     }
     
