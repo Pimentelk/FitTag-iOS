4224b59585dfd458d2dde7fac5b9dbcdfb723dd4
Kevin Pimentel <kevinpimentel@Kevins-Mac-mini.home>
2015-01-14 16:56:54 -0500

    Move initialization of image and labelText into the initWithFrameMethod: to fix new instance bug

--- FitTag/FTRewardsCollectionViewCell.m
+++ FitTag/FTRewardsCollectionViewCell.m
@@ -10,37 +10,42 @@
 
 @interface FTRewardsCollectionViewCell ()
 @property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UILabel *label;
 @end
 
 @implementation FTRewardsCollectionViewCell
 @synthesize imageView;
+@synthesize label;
 
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
         self.opaque = NO;
         self.clipsToBounds = YES;
+        
+        // Image
+        CGSize frameSize = self.frame.size;
+        imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, frameSize.width, frameSize.height - 35)];
+        [imageView setBackgroundColor: [UIColor clearColor]];
+        [self addSubview:imageView];
+        
+        // Label
+        label = [[UILabel alloc] initWithFrame:CGRectMake(0, frameSize.height - 35, frameSize.width, 35)];
+        label.textAlignment =  NSTextAlignmentCenter;
+        label.textColor = [UIColor blackColor];
+        label.backgroundColor = [UIColor whiteColor];
+        label.font = MULIREGULAR(22);
+        [self addSubview:label];
     }    
     return self;
 }
 
 - (void)setImage:(UIImage *)image {
-    imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, self.frame.size.width, self.frame.size.height - 35.0f)];
-    [imageView setBackgroundColor: [UIColor clearColor]];
     [imageView setImage:image];
-    
-    [self addSubview:imageView];
 }
 
 - (void)setLabelText:(NSString *)text {
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0.0f,self.frame.size.height - 35.0f,self.frame.size.width,35.0f)];
-    label.textAlignment =  NSTextAlignmentCenter;
-    label.textColor = [UIColor blackColor];
-    label.backgroundColor = [UIColor whiteColor];
-    label.font = BENDERSOLID(22);
     label.text = text;
-    
-    [self addSubview:label];
 }
 
 @end
